b3592e58405036d847e92702840f27ae0bdc1334
Saif Bashar <60818905+saifbashar@users.noreply.github.com>
2020-12-12 00:31:14 +0600

    Update Electronic Voting Machine Prototype Using C.c

--- Electronic Voting Machine Prototype Using C.c
+++ Electronic Voting Machine Prototype Using C.c
@@ -37,9 +37,9 @@ printf("\n thanks for vote !!");
 void votesCount(){
 printf("\n\n ##### Voting Statics ####");
 printf("\n %s - %d ", CANDIDATE1, votesCount1);
-printf("\n %s - %d ", CANDIDATE1, votesCount2);
-printf("\n %s - %d ", CANDIDATE1, votesCount3);
-printf("\n %s - %d ", CANDIDATE1, votesCount4);
+printf("\n %s - %d ", CANDIDATE2, votesCount2);
+printf("\n %s - %d ", CANDIDATE3, votesCount3);
+printf("\n %s - %d ", CANDIDATE4, votesCount4);
 printf("\n %s - %d ", "Spoiled Votes", spoiledtvotes); 
 }
 
